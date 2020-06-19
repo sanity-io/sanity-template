82b57d95bef223af23d1eebbad6968a1d65a5550
Bjørge Næss <bjoerge@gmail.com>
2020-06-19 15:52:15 +0200

    Support defining a studio base path

--- src/manifest/v2/vercel.ts
+++ src/manifest/v2/vercel.ts
@@ -2,6 +2,7 @@ import * as z from 'zod'
 import {sanityCorsOrigin} from './common'
 export const vercelDeployment = z.object({
   provider: z.literal('vercel'),
-  corsOrigins: z.array(sanityCorsOrigin).optional()
+  corsOrigins: z.array(sanityCorsOrigin).optional(),
+  studio: z.object({basePath: z.string()}).optional()
 })
 export type VercelDeployment = z.infer<typeof vercelDeployment>
