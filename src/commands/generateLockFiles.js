60ce9cc710c45175f85403f7ebd89cb51f83c692
Bjørge Næss <bjoerge@gmail.com>
2020-10-27 11:57:59 +0100

    Make generateLockFiles command handle v2 manifests

--- src/commands/generateLockFiles.js
+++ src/commands/generateLockFiles.js
@@ -1,3 +1,5 @@
+const {parseAsCurrent} = require('../index')
+
 const {kebabCase} = require('lodash')
 
 const {temporaryTransformPackage} = require('./utils/temporaryTransformPackage')
@@ -10,7 +12,23 @@ function generateLockFiles({basedir}) {
     throw new Error('Missing basedir')
   }
 
-  const manifest = require(path.join(basedir, 'sanity-template.json'))
+  const parseResult = parseAsCurrent(require(path.join(basedir, 'sanity-template.json')))
+
+  if (parseResult.type === 'invalid') {
+    throw new Error(
+      `Cannot generate lockfiles. The manifest found in ./sanity-template.json is invalid: ${parseResult.errors
+        .map(err => `${err.path.join('.')}: ${err.message}`)
+        .join('\n')}`
+    )
+  }
+
+  const manifest = parseResult.manifest
+
+  if (manifest.deployment.provider !== 'netlify') {
+    console.log('Not a netlify deployment, skipping lockfiles generation.')
+    return
+  }
+
   const prefix = kebabCase(manifest.title)
 
   const template = {
@@ -19,14 +37,14 @@ function generateLockFiles({basedir}) {
     path: path.resolve(basedir, 'template')
   }
 
-  const deployments = manifest.deployments.map(deployment => ({
+  const sites = manifest.deployment.sites.map(deployment => ({
     name: `${prefix}-${deployment.id}`,
     dir: deployment.dir,
     path: path.resolve(template.path, deployment.dir)
   }))
 
   return Promise.all(
-    [template, ...deployments].map(async info => {
+    [template, ...sites].map(async info => {
       const packagePath = path.join(info.path, 'package.json')
       const restorePkg = temporaryTransformPackage(packagePath, pkg => ({
         ...pkg,
