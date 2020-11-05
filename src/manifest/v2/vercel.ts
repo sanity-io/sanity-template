b8264dd40b58947261d566a72a98452acef180ee
Bjørge Næss <bjoerge@gmail.com>
2020-11-05 20:46:26 +0100

    Support specifying tokens for Vercel deployments

--- src/manifest/v2/vercel.ts
+++ src/manifest/v2/vercel.ts
@@ -1,8 +1,23 @@
 import * as z from 'zod'
-import {sanityCorsOrigin} from './common'
+import {sanityCorsOrigin, tokenSpec} from './common'
+
+const envVarMap = z.object({
+  projectId: z.array(z.string()),
+  dataset: z.array(z.string())
+})
+
 export const vercelDeployment = z.object({
   provider: z.literal('vercel'),
   corsOrigins: z.array(sanityCorsOrigin).optional(),
+  tokens: z
+    .array(
+      tokenSpec.extend({
+        envVar: z.string().optional()
+      })
+    )
+    .optional(),
+  // The env vars `SANITY_STUDIO_API_DATASET` and `SANITY_STUDIO_API_PROJECT_ID` will always be added
+  envVars: envVarMap.optional(),
   studio: z.object({basePath: z.string()}).optional()
 })
 export type VercelDeployment = z.infer<typeof vercelDeployment>
